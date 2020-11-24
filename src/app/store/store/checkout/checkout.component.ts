739a2939e43c4f70e677418da13ef543963a50d4
Azael Rivera <azaelkiller@gmail.com>
2020-11-23 18:41:52 -0700

    updated

--- src/app/store/store/checkout/checkout.component.ts
+++ src/app/store/store/checkout/checkout.component.ts
@@ -6,23 +6,21 @@ import { OrderRepository } from 'src/app/models/order.repository';
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
-  styleUrls: ['./checkout.component.css']
+  styleUrls: ['./checkout.component.css'],
 })
 export class CheckoutComponent implements OnInit {
-  orderSent:boolean=false;
-  submitted:boolean=false;
-  constructor(public _repository:OrderRepository,public _order:Order){}
-  submitOrder(form:NgForm){
-      this.submitted=true;
-      if(form.valid){
-          this._repository.saveOrder(this._order).subscribe((order)=>{
-              this._order.clear();
-              this.orderSent=true;
-              this.submitted=false;
-          });
-      }
+  orderSent: boolean = false;
+  submitted: boolean = false;
+  constructor(public _repository: OrderRepository, public _order: Order) {}
+  submitOrder(form: NgForm) {
+    this.submitted = true;
+    if (form.valid) {
+      this._repository.saveOrder(this._order).subscribe((order) => {
+        this._order.clear();
+        this.orderSent = true;
+        this.submitted = false;
+      });
+    }
   }
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
